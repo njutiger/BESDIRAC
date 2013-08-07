a531aede1b9162430cf66525b6f13646319ae27f
zhangxiaomei <zhangxm@ihep.ac.cn>
2013-08-07 16:53:12 +0800

    Add a RPC call.

--- Web/controllers/transfer/monitor.py
+++ Web/controllers/transfer/monitor.py
@@ -3,8 +3,23 @@
 from dirac.lib.base import *
 from dirac.lib.webBase import defaultRedirect
 import dirac.lib.credentials as credentials
+from dirac.lib.diset import getRPCClient
 
 class MonitorController(BaseController):
 
   def index(self):
     return render("/transfer/monitor.mako")
+
+  @jsonify
+  def reqs(self):
+    result_of_reqs = {'num': 0, 'data': []}
+    #username = str(credentials.getUsername())
+    RPC = getRPCClient("Transfer/TransferRequest")
+    res = RPC.status()
+    if not res["OK"]:
+      return result_of_reqs
+    result = res["Value"]
+    result_of_reqs["num"] = len(result)
+    result_of_reqs["data"] = result
+
+    return result_of_reqs

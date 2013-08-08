a123e114977899556c91b505ce2bb0674f3fca20
zhangxiaomei <zhangxm@ihep.ac.cn>
2013-08-08 20:07:45 +0800

    return a dict.

--- Web/controllers/transfer/monitor.py
+++ Web/controllers/transfer/monitor.py
@@ -5,6 +5,8 @@ from dirac.lib.webBase import defaultRedirect
 import dirac.lib.credentials as credentials
 from dirac.lib.diset import getRPCClient
 
+from BESDIRAC.TransferSystem.DB.TransferDB import TransRequestEntryWithID
+
 class MonitorController(BaseController):
 
   def index(self):
@@ -26,7 +28,7 @@ class MonitorController(BaseController):
     def quickconv(d):
       from  datetime import datetime
       # submit time is the 5th.
-      return tuple(r.strftime("%Y-%m-%d %H:%M [UTC]") if isinstance(r, datetime) else r for r in d )
+      return dict(zip(TransRequestEntryWithID._fields, tuple(r.strftime("%Y-%m-%d %H:%M [UTC]") if isinstance(r, datetime) else r for r in d )))
     result = map(quickconv, result)
 
     result_of_reqs["num"] = len(result)

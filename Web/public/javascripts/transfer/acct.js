6af89540654c4cbd6c39e70c0a0f971313043475
lintao <lintao51@gmail.com>
2013-09-13 19:56:55 +0800

    Must add these parts.

--- Web/public/javascripts/transfer/acct.js
+++ Web/public/javascripts/transfer/acct.js
@@ -12,7 +12,32 @@ function initAcctPlots( plotsList, selectionData ){
 
 function renderPage( plotsList, selectionData ){
   initPlotPage( "Data Transfer plot generation" );
-
+  // part 1
   appendToLeftPanel( createComboBox( "plotName", "Plot to generate", "Select a plot", plotsList ) );
+
+  // part 2
+  var orderKeys = [];
+  for( key in selectionData )
+  {
+  	orderKeys.push( [ key, key ] );
+  }
+  appendToLeftPanel( createComboBox( "grouping", "Group by", "Select grouping", orderKeys ) );
+
+  // part 3
+  appendTimeSelectorToLeftPanel();
+  // part 4
+  var selWidgets = []
+
+  selWidgets.push( createMultiselect( "User", "User", selectionData.User ) );
+  selWidgets.push( createMultiselect( "Source", "Source site", selectionData.Source ) );
+  selWidgets.push( createMultiselect( "Destination", "Destination site", selectionData.Destination ) );
+  selWidgets.push( createMultiselect( "Protocol", "Protocol", selectionData.Protocol ) );
+  selWidgets.push( createMultiselect( "FinalStatus", "Final transfer status", selectionData.FinalStatus ) );
+  selWidgets.push( createHidden( "typeName", "DataOperation" ) );
+  appendToLeftPanel( createPanel( "Selection conditions", selWidgets ) );
+
+  // part 5
+  appendAdvancedSettingsWidget();
+  // final
   renderPlotPage();
 }

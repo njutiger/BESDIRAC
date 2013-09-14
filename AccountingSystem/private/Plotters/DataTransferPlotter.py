28ec571048dcc4b1e9ca6e63b2d2ead9fe4b299b
lintao <lintao51@gmail.com>
2013-09-14 16:50:14 +0800

    Add throughput.

--- AccountingSystem/private/Plotters/DataTransferPlotter.py
+++ AccountingSystem/private/Plotters/DataTransferPlotter.py
@@ -31,3 +31,33 @@ class DataTransferPlotter( BaseReporter ):
                 }
     return self._generatePiePlot( filename, plotInfo[ 'data'], metadata )
 
+  def _reportThroughput( self, reportRequest ):
+    selectFields = ( self._getSelectStringForGrouping( reportRequest[ 'groupingFields' ] ) + ", %s, %s, SUM(%s)",
+                     reportRequest[ 'groupingFields' ][1] + [ 'startTime', 'bucketLength', 'TransferSize' ]
+                   )
+    retVal = self._getTimedData( reportRequest[ 'startTime' ],
+                                reportRequest[ 'endTime' ],
+                                selectFields,
+                                reportRequest[ 'condDict' ],
+                                reportRequest[ 'groupingFields' ],
+                                {} )
+    if not retVal[ 'OK' ]:
+      return retVal
+    dataDict, granularity = retVal[ 'Value' ]
+    self.stripDataField( dataDict, 0 )
+    dataDict, maxValue = self._divideByFactor( dataDict, granularity )
+    dataDict = self._fillWithZero( granularity, reportRequest[ 'startTime' ], reportRequest[ 'endTime' ], dataDict )
+    baseDataDict, graphDataDict, maxValue, unitName = self._findSuitableRateUnit( dataDict,
+                                                                                  self._getAccumulationMaxValue( dataDict ),
+                                                                                  "bytes" )
+    return S_OK( { 'data' : baseDataDict, 'graphDataDict' : graphDataDict,
+                   'granularity' : granularity, 'unit' : unitName } )
+
+  def _plotThroughput( self, reportRequest, plotInfo, filename ):
+    metadata = { 'title' : 'Throughput by %s' % reportRequest[ 'grouping' ] ,
+                 'ylabel' : plotInfo[ 'unit' ],
+                 'starttime' : reportRequest[ 'startTime' ],
+                 'endtime' : reportRequest[ 'endTime' ],
+                 'span' : plotInfo[ 'granularity' ] }
+    return self._generateTimedStackedBarPlot( filename, plotInfo[ 'graphDataDict' ], metadata )
+

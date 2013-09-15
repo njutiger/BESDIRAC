3520796292cfca9b6eb0553b3e80ed01e7a01871
lintao <lintao51@gmail.com>
2013-09-15 20:20:26 +0800

    Add retransfer in controller.

--- Web/controllers/transfer/reqmgr.py
+++ Web/controllers/transfer/reqmgr.py
@@ -44,3 +44,17 @@ class ReqmgrController(BaseController):
       return S_ERROR(str(e))
     RPC = getRPCClient("Transfer/TransferRequest")
     return RPC.delete({"id":fileid})
+
+  @jsonify
+  def retransfer(self):
+    """
+      This is to retransfer the file tranfer in one request.
+    """
+    if not request.params.has_key("id"):
+      return S_ERROR("Lack File ID")
+    try:
+      fileid = int(request.params["id"].encode("utf-8"))
+    except Exception as e:
+      return S_ERROR(str(e))
+    RPC = getRPCClient("Transfer/TransferRequest")
+    return RPC.retransfer({"id":fileid})
